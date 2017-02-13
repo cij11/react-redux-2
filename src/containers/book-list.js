2e7edef8465b39281916696ea1cdad9d1b5c20b5
Christopher Jolly <christopher.jolly@orionhealth.com>
2017-02-14 11:53:53 +1300

    Promoting book-list component to a container. Adding a mapping function to map the app state to the container props. Using connect to join them.

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'; //Word means pull the whole thing. {} means assign an element from that library as its own name (Eg, React.Component assigned to Component)
+import { connect }from 'react-redux'; //{} means just pull one element, and assign as its own name
 
-export default class BookList extends Component{
+class BookList extends Component{
 
   //Map over an array of books. For each element in the array, create a list item (li) that containts the book title.
   //Somehow, need to get the state from the reducer to here. This is done with react-redux library.
@@ -21,3 +22,19 @@ export default class BookList extends Component{
     )
   }
 }
+
+//Takes the application state as an arguement (here, the list of books, and the active book)
+//Whatever is returned will show up as props inside of booklist.
+//
+//While we hand the whole state in, we only assign the stuff that a container cares about to its props.
+function mapStateToProps(state){
+  //This return will be set to BookList.props
+  return{
+    books : state.books
+  };
+} //And again: The returned object is what will be avialable to BookList as props.
+
+//Export the container: Eg, the connect of maptStateToPRops and Booklist
+export default connect(mapStateToProps)(BookList);
+
+//Whenever application state changes, then container will rerender.
